ea99696f8ddf26a8a87de9f3d4664f1600c1f1b8
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-28 11:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const form = document.querySelector('form');
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const rememberMe = document.querySelector('#remember-me');
+
+		form.addEventListener('submit', function(event) {
+			event.preventDefault();
+
+			if (rememberMe.checked) {
+				localStorage.setItem('username', username.value);
+				localStorage.setItem('password', password.value);
+			} else {
+				localStorage.removeItem('username');
+				localStorage.removeItem('password');
+			}
+
+			alert('Logged in as ' + username.value);
+
+			if (localStorage.getItem('username')) {
+				const existingUserButton = document.createElement('button');
+				existingUserButton.id = 'existing';
+				existingUserButton.textContent = 'Login as existing user';
+				document.body.appendChild(existingUserButton);
+
+				existingUserButton.addEventListener('click', function() {
+					alert('Logged in as ' + localStorage.getItem('username'));
+				});
+			}
+		});
